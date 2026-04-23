46d1b71f31ef6e47ae4bc0c620b270c91adf4605
TonyDeHoyosH <243776@gmail.com>
2026-04-22 23:40:27 -0600

    fix: update CORS origins for production

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://taller-calendar-frontend.vercel.app',
+      'https://taller-calendar-frontend-emiliojaras3s-projects.vercel.app'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
